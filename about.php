d759a6eb9744123918cd1019f35996ef6dfa9e74
Juan <juandy_jc@yahoo.com>
2021-03-27 20:12:01 -0500

    agregado about e index .php

--- about.php
+++ about.php
@@ -1,1 +1,16 @@
-about
+<?php include("includes/header.php") ?>
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-12">
+      <h2>ACERCA DE</h2>
+      <h5>PAGINA DE PRUEBA EN DOCKER</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      
+    </div>
+  </div>
+</div>
+
+<?php include("includes/footer.php") ?>
--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-html
+
+<?php include("includes/header.php") ?>
+
+
+<div class="container" style="margin-top:30px">
+    <div class="row">
+      <div class="col-sm-4">
+        <h2>Datos</h2>
+        <h5>Espacio para foto</h5>
+        <div class="fakeimg">Fake Image</div>
+        <p>Mi nombre es Juan Camacho</p>
+        <p>Estudiante de 5to desarrollo de Sotware</p>
+      </div>
+      <div class="col-sm-8">
+        <h2>Sección</h2>
+        <h5>Descripción, Febrero, 2021</h5>
+        <div class="fakeimg">Fake Image</div>
+        <p>Algun texto..</p>
+        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati$
+      </div>
+    </div>
+  </div>
+  
+
+  <?php include("includes/footer.php") ?>
