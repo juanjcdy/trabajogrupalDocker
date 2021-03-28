335610a4e0b85e0477740d585a51d552f4c6728f
Juan <juandy_jc@yahoo.com>
2021-03-27 20:28:50 -0500

    creado el alrchivo crud.php

--- crud.php
+++ crud.php
@@ -1,1 +1,22 @@
 crud
+<?php include("db.php"); ?>
+
+<?php include('includes/header.php'); ?>
+
+<div class="container p-3 my-3 border">
+  <h1>Administrador de tareas</h1>
+</div>
+
+<main class="container p-4">
+  <div class="row">
+    <div class="col-md-4">
+      <!-- MESSAGES -->
+
+      <?php if (isset($_SESSION['message'])) { ?>
+      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
+        <?= $_SESSION['message']?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <?php session_unset(); } ?>
