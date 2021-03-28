0b0c3d00b166ea548a839666ca5b3966dfcf51eb
Juan Carlos Diaz <jc2876967@gmail.com>
2021-03-27 20:08:00 -0500

    modifico el archivo editar

--- edit.php
+++ edit.php
@@ -1,1 +1,21 @@
-edit
+<?php
+
+include("db.php");
+$title = '';
+$description= '';
+
+if  (isset($_GET['id'])) {
+  $id = $_GET['id'];
+  $query = "SELECT * FROM task WHERE id=$id";
+  $result = mysqli_query($conn, $query);
+  if (mysqli_num_rows($result) == 1) {
+    $row = mysqli_fetch_array($result);
+    $title = $row['title'];
+    $description = $row['description'];
+  }
+}
+
+
+
+
+?>
