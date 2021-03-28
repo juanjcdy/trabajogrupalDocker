f6c0a33349781911e0058bea9eaa20fd8392d511
Juan Carlos Diaz <jc2876967@gmail.com>
2021-03-27 20:21:38 -0500

    modificando el edit

--- edit.php
+++ edit.php
@@ -15,7 +15,17 @@ if  (isset($_GET['id'])) {
   }
 }
 
-
+if (isset($_POST['update'])) {
+    $id = $_GET['id'];
+    $title= $_POST['title'];
+    $description = $_POST['description'];
+  
+    $query = "UPDATE task set title = '$title', description = '$description' WHERE id=$id";
+    mysqli_query($conn, $query);
+    $_SESSION['message'] = 'Tarea actualizada';
+    $_SESSION['message_type'] = 'warning';
+    header('Location: crud.php');
+  }
 
 
 ?>
