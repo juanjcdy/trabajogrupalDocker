d53cfad399708fd680d9a6442a0e9b8d0a822f2a
Juan Carlos Diaz <jc2876967@gmail.com>
2021-03-27 20:32:39 -0500

    agregando formulario de editar

--- edit.php
+++ edit.php
@@ -29,3 +29,25 @@ if (isset($_POST['update'])) {
 
 
 ?>
+
+<?php include('includes/header.php'); ?>
+<div class="container p-4">
+  <div class="row">
+    <div class="col-md-4 mx-auto">
+      <div class="card card-body">
+      <form action="edit.php?id=<?php echo $_GET['id']; ?>" method="POST">
+        <div class="form-group">
+          <input name="title" type="text" class="form-control" value="<?php echo $title; ?>" placeholder="Actualizar Título">
+        </div>
+        <div class="form-group">
+        <textarea name="description" class="form-control" cols="30" rows="10"><?php echo $description;?></textarea>
+        </div>
+        <button class="btn-success" name="update">
+          Actualizar
+        </button>
+      </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php include('includes/footer.php'); ?>
